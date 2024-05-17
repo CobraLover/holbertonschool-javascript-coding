2bff95a8a80a155a4e2820c6d286d0699c24d0b6
CobraLover <6716@holbertonstudents.com>
2024-05-17 22:34:41 +0200

    Task.0-6

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const file = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(file, content, function (error) {
-  if (error) {
-    console.log(error);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
