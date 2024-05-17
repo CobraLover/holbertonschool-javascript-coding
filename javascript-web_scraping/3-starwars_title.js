c03db79d5baf3430333139a00d5e70e9e8ec63cf
CobraLover <6716@holbertonstudents.com>
2024-05-17 22:39:32 +0200

    Task.0-6

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const movie = JSON.parse(body);
-    console.log(movie.title);
-  }
+const url = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
