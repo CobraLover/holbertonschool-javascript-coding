a8b1c7e7e23a5eb3ed9c0cb2c2c26b0987339ae1
CobraLover <6716@holbertonstudents.com>
2024-05-17 22:12:35 +0200

    Task.0-6

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,21 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-FileContent(filePath);
-function FileContent (filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(data);
-  });
+
+// Check if file path is provided
+if (process.argv.length < 3) {
+  console.log('Usage: node 0-readme.js <file_path>');
+  process.exit(1);
 }
+
+const filePath = process.argv[2];
+
+// Read the content of the file
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  // Print the error object if an error occurred
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
